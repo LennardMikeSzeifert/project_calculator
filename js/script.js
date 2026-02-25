6e8e50a8f9b4bda61bb4dc7362c345d0180a668d
LennardMikeSzeifert <lennard.mike.szeifert@gmail.com>
2026-02-25 16:00:43 +0100

    feat(calculator): implement reset button
    - attach click handler to reset button
    - clear all elements from display
    
    Refs: projects/calculator/
    Authored-by: Lennard

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ elementEquals.textContent = `=`;
 elementEquals.className = `calculator-element`;
 const elementResult = document.createElement(`p`);
 elementResult.className = `calculator-element`;
+const resetBtn = document.querySelector(`.reset`);
 
 const add = function (a, b) {
   return +a + +b;
@@ -146,4 +147,12 @@ equals.addEventListener(`click`, function displayResult() {
   resultDisplay.appendChild(elementEquals);
   elementResult.textContent = `${operate(num1, operator, num2)}`;
   resultDisplay.appendChild(elementResult);
+
+  resetBtn.addEventListener(`click`, function removeDisplayElements() {
+    let calculatorElements = document.querySelectorAll(`.calculator-element`);
+    num1 = ``;
+    num2 = ``;
+    operator = ``;
+    calculatorElements.forEach((element) => element.remove());
+  });
 });
